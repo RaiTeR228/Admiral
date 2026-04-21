55baf11295bb9625c0bfc4c8a5f18d6ef94882e5
RaiTer228 <doshkcemcemcem@gmail.com>
2026-04-21 10:42:32 +0500

    Создана новая бд, тк в прошлой были проблемы с запуском проекта

---
+++ config/SSHLog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SshlogConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "SSHLog"
--- models/server/models.py
+++ models/server/models.py
@@ -30,18 +30,18 @@ class Server(models.Model):
     def __str__(self):
         return self.name
     
-class List_Ssh(models.Model):
-    # Accepted password for root from 192.168.1.219 port 37327 ssh2
-    # 2026-03-27T17:28:10.658373+00:00 Scan-ingram sshd[19328]: Failed password for root from 192.168.1.219 port 37625 ssh2
-    # 2026-03-27T17:28:10.658373+00:00 Scan-ingram sshd[19328]: Failed password for root from 192.168.1.219 port 37625 ssh2
-    # 2026-03-27T17:28:59.002414+00:00 Scan-ingram sshd[19328]: message repeated 2 times: [ Failed password for root from 192.168.1.219 port 37625 ssh2]
-    # 2026-03-27T17:28:59.666846+00:00 Scan-ingram sshd[19328]: Connection reset by authenticating user root 192.168.1.219 port 37625 [preauth]
-    # 2026-03-27T17:28:59.667394+00:00 Scan-ingram sshd[19328]: PAM 2 more authentication failures; logname= uid=0 euid=0 tty=ssh ruser= rhost=192.168.1.219  user=root
+# class List_Ssh(models.Model):
+#     # Accepted password for root from 192.168.1.219 port 37327 ssh2
+#     # 2026-03-27T17:28:10.658373+00:00 Scan-ingram sshd[19328]: Failed password for root from 192.168.1.219 port 37625 ssh2
+#     # 2026-03-27T17:28:10.658373+00:00 Scan-ingram sshd[19328]: Failed password for root from 192.168.1.219 port 37625 ssh2
+#     # 2026-03-27T17:28:59.002414+00:00 Scan-ingram sshd[19328]: message repeated 2 times: [ Failed password for root from 192.168.1.219 port 37625 ssh2]
+#     # 2026-03-27T17:28:59.666846+00:00 Scan-ingram sshd[19328]: Connection reset by authenticating user root 192.168.1.219 port 37625 [preauth]
+#     # 2026-03-27T17:28:59.667394+00:00 Scan-ingram sshd[19328]: PAM 2 more authentication failures; logname= uid=0 euid=0 tty=ssh ruser= rhost=192.168.1.219  user=root
 
-    server = models.CharField(max_length=255)
-    timestamp = models.DateTimeField()
-    ip = models.GenericIPAddressField()
-    port = models.IntegerField()
-    username = models.CharField(max_length=255)
-    status = models.CharField(max_length=255)  # "Accepted" or "Failed"
-    full_message_log = models.TextField() # полный текст из лога
+#     server = models.CharField(max_length=255)
+#     timestamp = models.DateTimeField()
+#     ip = models.GenericIPAddressField()
+#     port = models.IntegerField()
+#     username = models.CharField(max_length=255)
+#     status = models.CharField(max_length=255)  # "Accepted" or "Failed"
+#     full_message_log = models.TextField() # полный текст из лога
